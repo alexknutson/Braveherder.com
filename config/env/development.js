7fb0a84c1274305504e75e6f93d1ece1378bc497
Alex Knutson <alexknutson@gmail.com>
2014-11-11 23:08:39 -0600

    Google Analytics, fix title for realz

--- config/env/development.js
+++ config/env/development.js
@@ -3,7 +3,7 @@
 module.exports = {
 	db: 'mongodb://localhost/dune-dev',
 	app: {
-		title: 'BraverHerder - Development Environment'
+		title: 'BraveHerder - Beta'
 	},
 	facebook: {
 		clientID: process.env.FACEBOOK_ID || 'APP_ID',
